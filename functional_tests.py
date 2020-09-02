706b19dc65ed04e28c30792c6578b68e72fe7adf
jenessaseymour <jenessaseymour@users.noreply.github.com>
2020-09-01 23:02:56 -0400

    Update functional_tests.py
    Chapter 2 updates

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class BasicVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_click_a_link_to_a_recipe(self):
+        #open the site
+        self.browser.get('http://localhost:8000')
+
+        #see the homepage title
+        self.assertIn('Recipe Book', self.browser.title)
+        self.fail('Finish the test!')
+
+        #see the list of recipes in a column
+
+        #clicking on a recipe name pulls up that recipe
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
